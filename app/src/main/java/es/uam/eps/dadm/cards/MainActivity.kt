8e43670c96933a5b5abf4a54fcf072c4d75eb9f7
Jaime <jlopezbarajas@gmail.com>
2022-02-28 20:57:38 +0100

    RECURSOS

--- app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
@@ -2,10 +2,16 @@ package es.uam.eps.dadm.cards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = resources.getString(R.string.app_name)
+
+        // Añade aquí el código para crear el Toast
+        Toast.makeText(this, name, Toast.LENGTH_LONG).show()
     }
 }
