0dee744846576e0064abc89f01c96578565756b6
Jaime <jlopezbarajas@gmail.com>
2022-03-01 12:33:53 +0100

    CAP-addCard - Añadida tarjeta y vista de los textos

--- app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
@@ -11,17 +11,25 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     private lateinit var answerButton: Button
     private lateinit var answerTextView: TextView
+    private lateinit var questionTextView: TextView
     private lateinit var difficultyButtons: LinearLayout
     private lateinit var separadorView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var card = Card("TEST", "PRUEBA")
         setContentView(R.layout.activity_main)
 
+
+
         answerButton = findViewById(R.id.answer_button)
+        questionTextView = findViewById(R.id.question_text_view)
         answerTextView = findViewById(R.id.answer_text_view)
         difficultyButtons = findViewById(R.id.difficulty_buttons)
         separadorView = findViewById(R.id.separator_view)
+
+        questionTextView.text = card.question
+        answerTextView.text = card.answer
 
         answerButton.setOnClickListener {
             answerTextView.visibility = View.VISIBLE
