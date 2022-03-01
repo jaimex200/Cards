96583763ffd67c9befa35e2b9acf13dfb3f410ad
Jaime <jlopezbarajas@gmail.com>
2022-03-01 12:20:00 +0100

    Add Cards and Stadistics. Update gradle version

--- app/src/main/java/es/uam/eps/dadm/cards/Card.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/Card.kt
@@ -1,4 +1,121 @@
 package es.uam.eps.dadm.cards
 
-class Card {
+import java.time.*
+import java.util.UUID
+import kotlin.math.roundToLong
+
+open class Card(
+    var question: String,
+    var answer: String,
+    var quality: Int = 0,
+    var repetitions: Int = 0,
+    var interval: Long = 1L,
+    var nextPracticeDate: String = LocalDateTime.now().toString(),
+    var easiness: Double = 2.5,
+    var date: String = LocalDateTime.now().toString(),
+    var id: String = UUID.randomUUID().toString()
+) {
+    companion object Load{
+        fun fromString(cad: String): Card {
+            val data = cad.replace(" ","").split("|")
+            val card = Card(data[1], data[2])
+
+            card.quality = data[3].toInt()
+            card.repetitions = data[4].toInt()
+            card.interval = data[5].toLong()
+            card.nextPracticeDate = data[6]
+            card.easiness = data[7].toDouble()
+            card.date = data[8]
+            card.id = data[9]
+
+            return card
+        }
+    }
+
+    /**Funcion para hacer una tarjeta*/
+    open fun show () {
+        print(" $question (INTRO para ver respuesta) ")
+        readLine()
+        print(" $answer (Teclea 0, 3 o 5): ")
+        val userAns = readLine()?.toIntOrNull() ?: -1
+        quality = userAns
+    }
+
+    open fun defaultToString() =
+        "$question | $answer | $quality | $repetitions | $interval | $nextPracticeDate | $easiness | $date | $id \n"
+
+    override fun toString() = "card | " + defaultToString()
+
+    /**Funcion para enseñar la tarjeta*/
+    open fun showCard () {
+        println("$question -> $answer")
+    }
+
+    /**Funcion para ejecutar la tarjeta*/
+    fun execCard (currentDate: LocalDateTime) {
+        if (currentDate.toString().split('T')[0] == nextPracticeDate.split('T')[0]) {
+            show()
+            update(currentDate)
+            details()
+        }
+    }
+
+    /**Funcion para actualizart la tarjeta*/
+    private fun update (currentDate: LocalDateTime) {
+        easinessAlg()
+        repetitionAlg()
+        intervalAlg()
+        updateStats()
+
+        nextPracticeDate = currentDate.plusDays(interval).toString()
+    }
+
+    /**Funcion para actualizar las estadisticas*/
+    private fun updateStats() {
+        when(quality) {
+            0 -> Stadistics.addError()
+            3 -> Stadistics.adddoubt()
+            else -> Stadistics.addSuccess()
+        }
+
+        if (easiness == 1.3) Stadistics.addCard(this) else Stadistics.deleteCard(this)
+    }
+
+    /**Funcion para enseñar los detalles de la tarjeta*/
+    private fun details () {
+        println(" eas = ${"%.2f".format(easiness)} rep = $repetitions int = $interval next = ${nextPracticeDate.split('T')[0]}")
+    }
+
+    /**Simulacion de la tarjeta*/
+    fun simulate(period: Long) {
+        println("Simulación de la tarjeta $question:")
+        var now = LocalDateTime.now()
+
+        for (p in 1..period) {
+            println("Fecha: $now")
+            if (now.toString().split('T')[0] == nextPracticeDate.split('T')[0]) {
+                show()
+                update(now)
+            }
+            now = now.plusDays(1)
+        }
+    }
+
+    /**Funcion auxiliar*/
+    private fun easinessAlg () {
+        val easinessAlg = easiness + 0.1 - (5.0 - quality) * (0.08 + (5.0 - quality) * 0.02)
+
+        easiness = maxOf(easinessAlg, 1.3)
+
+    }
+
+    /**Funcion auxiliar*/
+    private fun repetitionAlg () {
+        if (quality < 3) repetitions = 0 else repetitions += 1
+    }
+
+    /**Funcion auxiliar*/
+    private fun intervalAlg () {
+        interval = if (repetitions <= 1) 1 else if (repetitions == 2) 6 else (interval.toDouble() * easiness).roundToLong()
+    }
 }
---
+++ app/src/main/java/es/uam/eps/dadm/cards/Stadistics.kt
@@ -0,0 +1,37 @@
+package es.uam.eps.dadm.cards
+
+object Stadistics {
+    var success: Int = 0
+    var doubt: Int = 0
+    var error: Int = 0
+    var listCards: MutableList<Card> = mutableListOf()
+    fun show () {
+
+        if (success != 0) println("Porcentaje de aciertos: ${(success.toDouble()/(success+error+doubt))*100}%") else println("Porcentaje de aciertos: 0%" )
+
+        println("Tarjetas mas dificiles")
+        for (c in listCards) {
+            c.showCard()
+        }
+    }
+
+    fun addSuccess () {
+        success += 1
+    }
+
+    fun adddoubt () {
+        doubt += 1
+    }
+
+    fun addError() {
+        error += 1
+    }
+
+    fun addCard(c: Card) {
+        if (listCards.contains(c)) return else listCards.add(c)
+    }
+
+    fun deleteCard(c: Card) {
+        if (listCards.contains(c)) listCards.remove(c)
+    }
+}
