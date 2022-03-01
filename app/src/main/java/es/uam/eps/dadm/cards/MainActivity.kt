7a50eac0fcff5c367c325cfcc6ed76accec039f8
Jaime <jlopezbarajas@gmail.com>
2022-03-01 11:45:18 +0100

    Escuchadores P5

--- app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var answerButton: Button
     private lateinit var answerTextView: TextView
+    private lateinit var difficultyButtons: LinearLayout
+    private lateinit var separadorView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         answerButton = findViewById(R.id.answer_button)
         answerTextView = findViewById(R.id.answer_text_view)
+        difficultyButtons = findViewById(R.id.difficulty_buttons)
+        separadorView = findViewById(R.id.separator_view)
 
         answerButton.setOnClickListener {
             answerTextView.visibility = View.VISIBLE
             answerButton.visibility = View.INVISIBLE
+            difficultyButtons.visibility = View.VISIBLE
+            separadorView.visibility = View.VISIBLE
         }
     }
 }
