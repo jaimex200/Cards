730cbbfb15b180700f493e464170a2b8b5dfd505
Jaime <jlopezbarajas@gmail.com>
2022-03-18 11:21:45 +0100

    ejercicio/ViewModel - Persistencia de viewmodel

--- app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/MainActivity.kt
@@ -12,20 +12,38 @@ private const val ANSWERED_KEY = "es.uam.eps.dadm.cards:answered"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        Timber.i("$viewModel")
+
         binding.card = viewModel.card
-        binding.apply {
-            answerButton.setOnClickListener {
-                card?.answered = true
-                invalidateAll()
-            }
+        binding.answerButton.setOnClickListener {
+            viewModel.card?.answered = true
+            binding.invalidateAll()
         }
+        Timber.i("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume called")
+        binding.invalidateAll()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause called")
     }
 
     override fun onStop() {
