017e1172e2d04b72cf325bf1857823361f1b6460
Jaime <jlopezbarajas@gmail.com>
2022-03-26 13:07:20 +0100

    ejercicio/Intenciones -> Declaracion implicita

--- app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
@@ -11,7 +11,13 @@ class TitleActivity : AppCompatActivity() {
         setContentView(R.layout.activity_title)
 
         findViewById<TextView>(R.id.cards_title_text_view).setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            //startActivity(Intent(this, MainActivity::class.java))
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setType("text/plain")
+            intent.putExtra("MESSAGE", "Saludos")
+            startActivity(intent)
         }
+
+
     }
 }
