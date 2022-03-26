c272c7bd3f2615d2ac6821777c8ab1f5b3794e79
Jaime <jlopezbarajas@gmail.com>
2022-03-26 13:20:41 +0100

    ejercicio/Intenciones -> Vinculacion del binding TitleActivity.kt

--- app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
@@ -4,20 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import es.uam.eps.dadm.cards.databinding.ActivityTitleBinding
 
 class TitleActivity : AppCompatActivity() {
+    lateinit var binding: ActivityTitleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
-
-        findViewById<TextView>(R.id.cards_title_text_view).setOnClickListener {
-            //startActivity(Intent(this, MainActivity::class.java))
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.setType("text/plain")
-            intent.putExtra("MESSAGE", "Saludos")
-            startActivity(intent)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_title)
+        binding.cardsTitleTextView.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
-
-
     }
 }
