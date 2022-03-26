ac3c0ec36b4caec08576dcb697640d99d13168b4
Jaime <jlopezbarajas@gmail.com>
2022-03-26 13:05:19 +0100

    ejercicio/Intenciones -> Creado pagina title e implementacion para pasar a mainactivity

---
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
@@ -0,0 +1,17 @@
+package es.uam.eps.dadm.cards
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class TitleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_title)
+
+        findViewById<TextView>(R.id.cards_title_text_view).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
