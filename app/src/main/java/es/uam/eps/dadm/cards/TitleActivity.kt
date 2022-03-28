225fe1a62c2c64a0440e0889ac36d77765908498
Jaime <jlopezbarajas@gmail.com>
2022-03-28 15:26:48 +0200

    ejercicio/navegacion_fragmentos -> funcionalidad back

--- app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
+++ app/src/main/java/es/uam/eps/dadm/cards/TitleActivity.kt
@@ -14,6 +14,7 @@ class TitleActivity : AppCompatActivity(), TitleFragment.OnTitleFragmentInteract
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, StudyFragment())
+            .addToBackStack("onStudy")
             .commit()
 
     }
